39828d52bad131cb503df805c405f6bd9e226264
s.kolesnik <7fosGbc9S>
2021-06-14 19:12:48 +0500

    added dependency installation shell scripts

---
+++ scripts/debian_install_deps.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+echo "installing dependencies"
+apt-get --assume-yes install libsdl2-dev \
+libglfw3-dev
---
+++ scripts/pacman_install_deps.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/bash
+echo "Installing dependencies with pacman..."
+pacman --noconfirm --needed -S mingw-w64-x86_64-SDL2 \
+mingw-w64-x86_64-glfw
